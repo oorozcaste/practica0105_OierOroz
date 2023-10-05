e6e7cfda938b062624cde1ca48eba1c08a0677ed
oorozcaste <145026396+oorozcaste@users.noreply.github.com>
2023-10-05 15:23:39 +0200

    ff
    hhhhh

---
+++ ejercicio6.py
@@ -0,0 +1,3 @@
+n = int(input("introduzca numero entero: "))
+resultado = ((n*(n+1))/2)
+print("la suma de los numeros enteros desde 1 hasta",n,"=",resultado)
---
+++ ejercicio7.py
@@ -0,0 +1,6 @@
+masa = float(input("indique aqui su peso en kg: "))
+altura = float(input("indique aqui su altura en metros: "))
+
+imc = masa/(altura**2)
+
+print("tu indice de masa corporal es: ",imc)
